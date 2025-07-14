d44e78068d801a7643dd1200fbf2e889239ae4aa
김태훈 <kth4909@evsis.co.kr>
2025-07-15 00:57:04 +0900

    feat: UserEntity 생성 시 입력값 검증 로직 및 테스트 추가

--- apps/commerce-api/src/main/java/com/loopers/domain/example/UserEntity.java
+++ apps/commerce-api/src/main/java/com/loopers/domain/example/UserEntity.java
@@ -1,6 +1,7 @@
 package com.loopers.domain.example;
 
-import java.time.LocalDate;
+import com.loopers.support.error.CoreException;
+import com.loopers.support.error.ErrorType;
 import lombok.Builder;
 import lombok.Getter;
 
@@ -11,10 +12,22 @@ public class UserEntity  {
     private final String userId;
     private final String email;
     private final String name;
-    private final LocalDate birthDate;
+    private final String birthDate;
 
     @Builder
-    public UserEntity(String userId, String email, String name, LocalDate birthDate) {
+    public UserEntity(String userId, String email, String name, String birthDate) {
+        if (userId == null || !userId.matches("^[a-zA-Z0-9]{1,10}$")) {
+            throw new CoreException(ErrorType.BAD_REQUEST);
+        }
+
+        if (email == null || !email.matches("^[\\w.-]+@[\\w-]+\\.[a-zA-Z]{2,}$")) {
+            throw new CoreException(ErrorType.BAD_REQUEST);
+        }
+
+        if (birthDate == null || !birthDate.matches("^\\d{4}-\\d{2}-\\d{2}$")) {
+            throw new CoreException(ErrorType.BAD_REQUEST);
+        }
+
         this.userId = userId;
         this.email = email;
         this.name = name;
