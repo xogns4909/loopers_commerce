a118eb1380101c07c9f0f5724ac98171c69ebea0
김태훈 <kth4909@evsis.co.kr>
2025-07-15 22:49:53 +0900

    refactor: User 생성 테스트 constructure -> Of 메소드로 변경

--- apps/commerce-api/src/test/java/com/loopers/domain/example/UserEntityTest.java
+++ apps/commerce-api/src/test/java/com/loopers/domain/example/UserEntityTest.java
@@ -1,9 +1,11 @@
 package com.loopers.domain.example;
 
 
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
 import com.loopers.domain.example.user.UserEntity;
 import com.loopers.support.error.CoreException;
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
@@ -15,62 +17,57 @@ public class UserEntityTest {
     @DisplayName("User가 정상적으로 생성된다.")
     void createUser_success() {
         // given
-        UserEntity user = UserEntity.builder()
-            .userId("kth4909")
-            .email("xogns4949@naver.com")
-            .gender("M")
-            .birthDay("1999-10-23")
-            .build();
+        UserEntity user = UserEntity.of("kth4909","xogns4949@naver.com","M","1999-10-23");
 
-        // when & then
-        Assertions.assertEquals("kth4909", user.getUserId());
-        Assertions.assertEquals("xogns4949@naver.com", user.getEmail());
-        Assertions.assertEquals("M", user.getGender());
-        Assertions.assertEquals("1999-10-23", user.getBirthDay());
+        // when then
+        assertThat(user.toString()).contains("kth4909", "xogns4949@naver.com", "M", "1999-10-23");
     }
 
     @DisplayName("유효하지 않은 ID 형식이면 예외가 발생한다")
     @ParameterizedTest
     @ValueSource(strings = {"invalidId1!", "toooolongusername", "abc@"})
-    void user_invalidId(String invalidId){
+    void invalidUserId(String invalidId) {
+        // given
         String email = "xogns4949@naver.com";
         String gender = "M";
         String birthDay = "1999-10-23";
 
         // when & then
-        Assertions.assertThrows(CoreException.class, () -> {
-            new UserEntity(invalidId, email, gender, birthDay);
-        });
-    }
+        assertThatThrownBy(() -> UserEntity.of(invalidId, email, gender, birthDay))
+            .isInstanceOf(CoreException.class)
+            .hasMessage("유효하지 않은 아이디입니다.");
+        }
+
 
     @DisplayName("유효하지 않은 메일 형식이면 예외가 발생한다")
     @ParameterizedTest
     @ValueSource(strings = {"plainemail", "user@.com", "user@domain", "user@naver..com"})
     void user_invalidEmail(String invalidEmail){
+        // given
         String id = "xogns4909";
-        String name = "김태훈";
+        String gender = "김태훈";
         String birthDay = "1999-10-23";
 
 
         // when & then
-        Assertions.assertThrows(CoreException.class, () -> {
-            new UserEntity(id, invalidEmail, name, birthDay);
-        });
+        assertThatThrownBy(() -> UserEntity.of(id, invalidEmail, gender, birthDay))
+            .isInstanceOf(CoreException.class)
+            .hasMessage("유효하지 않은 이메일 형식입니다.");
     }
 
     @DisplayName("유효하지 않은 생년월일 포맷이면 예외가 발생한다")
     @ParameterizedTest
     @ValueSource(strings = {"19990101", "99-10-23", "10-23-1999", "abcd-ef-gh"})
-    void user_invalidBirthDat(String invalidBirthDate) {
+    void user_invalidBirthDay(String invalidBirthDay) {
         // given
         String id = "xogns4909";
         String email = "xogns4949@naver.com";
         String gender = "M";
 
         // when & then
-        Assertions.assertThrows(CoreException.class, () -> {
-            new UserEntity(id, email, gender, invalidBirthDate);
-        });
+        assertThatThrownBy(() -> UserEntity.of(id, email, gender, invalidBirthDay))
+            .isInstanceOf(CoreException.class)
+            .hasMessage("유효하지 않은 생년월일 형식입니다.");
     }
 
 
