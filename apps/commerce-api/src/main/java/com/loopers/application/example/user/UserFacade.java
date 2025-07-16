8afa1186d64d60c1a65d3644ab4fb4802496ef6e
김태훈 <kth4909@evsis.co.kr>
2025-07-16 14:28:48 +0900

    feat : 유저 정보 조회 컨트롤러/퍼사드 추가 및 E2E 테스트 진행

--- apps/commerce-api/src/main/java/com/loopers/application/example/user/UserFacade.java
+++ apps/commerce-api/src/main/java/com/loopers/application/example/user/UserFacade.java
@@ -1,9 +1,15 @@
 package com.loopers.application.example.user;
 
+import com.loopers.domain.example.user.model.User;
+import com.loopers.domain.example.user.service.UserFindService;
 import com.loopers.domain.example.user.service.UserLoginService;
 import com.loopers.interfaces.api.user.LoginRequest;
 import com.loopers.interfaces.api.user.RegisterUserRequest;
 import com.loopers.domain.example.user.service.UserRegisterService;
+import com.loopers.interfaces.api.user.UserInfoRequest;
+import com.loopers.support.error.CoreException;
+import com.loopers.support.error.ErrorType;
+import java.util.Optional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 
@@ -15,11 +21,19 @@ public class UserFacade {
 
     private final UserLoginService userLoginService;
 
+    private final UserFindService userFindService;
+
     public UserResponse register(RegisterUserRequest userRequest) {
         return userRegisterService.register(userRequest.toCommand());
     }
 
     public UserResponse login(LoginRequest loginRequest) {
         return userLoginService.login(loginRequest.userId());
+    }
+
+    public UserResponse getUserInfo(UserInfoRequest request) {
+        return Optional.ofNullable(userFindService.findByUserId(request.userId()))
+            .map(UserResponse::from)
+            .orElseThrow(() -> new CoreException(ErrorType.NOT_FOUND, "존재하지 않는 유저입니다."));
     }
 }
