9bd5b4ee12b7df8ca7bdde24ad9147ed9247a115
김태훈 <kth4909@evsis.co.kr>
2025-07-16 18:04:58 +0900

    refactor : 포인트 없는 회원 포인트 조회시 0원 응답하도록 변경

--- apps/commerce-api/src/main/java/com/loopers/application/example/point/PointFacade.java
+++ apps/commerce-api/src/main/java/com/loopers/application/example/point/PointFacade.java
@@ -1,13 +1,8 @@
 package com.loopers.application.example.point;
 
-import com.loopers.application.example.user.UserResponse;
-import com.loopers.domain.example.point.model.service.PointAddService;
 import com.loopers.domain.example.point.service.PointFindService;
-import com.loopers.interfaces.api.point.PointInfoRequest.PointInfoRequest;
 import com.loopers.interfaces.api.point.PointResponse;
-import com.loopers.interfaces.api.user.UserInfoRequest;
-import com.loopers.support.error.CoreException;
-import com.loopers.support.error.ErrorType;
+import java.math.BigDecimal;
 import java.util.Optional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
@@ -18,9 +13,9 @@ public class PointFacade {
 
     private final PointFindService pointFindService;
 
-    public PointResponse findPointInfo(PointInfoRequest request) {
-        return Optional.ofNullable(pointFindService.findByUserId(request.userId()))
+    public PointResponse findPointInfo(String request) {
+        return Optional.ofNullable(pointFindService.findByUserId(request))
             .map(PointResponse::from)
-            .orElseThrow(() -> new CoreException(ErrorType.NOT_FOUND, "존재하지 않는 유저입니다."));
+            .orElseGet(() -> new PointResponse(request, BigDecimal.ZERO));
     }
 }
