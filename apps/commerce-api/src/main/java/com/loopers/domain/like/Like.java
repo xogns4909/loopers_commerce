0c5f67f1774b99ff981b955a354d0e46a3e7391a
김태훈 <kth4909@evsis.co.kr>
2025-07-29 01:05:00 +0900

    feat : like 객체 정의 및 생성 테스트 추가

---
+++ apps/commerce-api/src/main/java/com/loopers/domain/like/Like.java
@@ -0,0 +1,14 @@
+package com.loopers.domain.like;
+
+import com.loopers.support.error.CoreException;
+import com.loopers.support.error.ErrorType;
+
+public record Like(Long id, Long userId, Long productId) {
+
+    public static Like of(Long id, Long userId, Long productId) {
+        if (userId == null || productId == null) {
+            throw new CoreException(ErrorType.BAD_REQUEST);
+        }
+        return new Like(id, userId, productId);
+    }
+}
