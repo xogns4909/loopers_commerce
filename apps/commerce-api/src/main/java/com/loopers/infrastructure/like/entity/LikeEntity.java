c9d6dfc544afc1e5175fad16e67c5dc833919bff
김태훈 <kth4909@evsis.co.kr>
2025-08-01 01:18:28 +0900

    feat: 좋아요 도메인 및 서비스 로직 구현
    - LikeService 구현
    - LikeRepository 인터페이스 및 구현체 작성
    - 비즈니스 로직 단위 테스트 추가 (LikeServiceTest)

--- apps/commerce-api/src/main/java/com/loopers/infrastructure/like/entity/LikeEntity.java
+++ apps/commerce-api/src/main/java/com/loopers/infrastructure/like/entity/LikeEntity.java
@@ -1,20 +1,26 @@
 package com.loopers.infrastructure.like.entity;
 
 import com.loopers.domain.BaseEntity;
-
-import com.loopers.domain.user.model.UserId;
+import com.loopers.domain.like.model.Like;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Table;
 import java.time.LocalDateTime;
-
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
 
 @Entity
 @Table(name = "likes")
+@NoArgsConstructor
+@AllArgsConstructor
 public class LikeEntity extends BaseEntity {
 
-    private LocalDateTime likeAt;
+    private String userId;
 
     private Long productId;
 
-    private String userId;
+    private LocalDateTime likeAt;
+
+    public static LikeEntity from(Like like) {
+        return  new LikeEntity(like.userId().value(),like.productId(),LocalDateTime.now());
+    }
 }
