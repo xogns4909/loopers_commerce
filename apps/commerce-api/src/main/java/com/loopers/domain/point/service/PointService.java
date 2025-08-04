6e359324535bd9be0ae15380f700812bc70e1db3
김태훈 <kth4909@evsis.co.kr>
2025-08-04 21:18:36 +0900

    refactor : 포인트/유저 어플리케이션 레이어 통합 및 테스트 수정

--- apps/commerce-api/src/main/java/com/loopers/domain/point/service/PointService.java
+++ apps/commerce-api/src/main/java/com/loopers/domain/point/service/PointService.java
@@ -1,9 +1,14 @@
 package com.loopers.domain.point.service;
 
+import com.loopers.application.point.AddPointCommand;
 import com.loopers.domain.payment.model.PaymentAmount;
+import com.loopers.domain.point.model.Point;
 import com.loopers.domain.user.model.UserId;
+import com.loopers.interfaces.api.point.PointResponse;
 
 public interface PointService {
     boolean hasEnough(UserId userId, PaymentAmount amount);
     void deduct(UserId userId, PaymentAmount amount);
+    Point findByUserId(String userId);
+    PointResponse charge(AddPointCommand addPointCommand);
 }
