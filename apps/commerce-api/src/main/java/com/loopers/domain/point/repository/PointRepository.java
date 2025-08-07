91905e345a93449cafbd6b80580a35266225a3e5
김태훈 <kth4909@evsis.co.kr>
2025-08-07 15:50:51 +0900

    feat: 동시성 제어 적용 및 포인트 통합 테스트 추가
    - `HandleConcurrency` 애너테이션 도입 및 충전/차감 로직에 낙관적 락 적용
    - `PointConcurrencyIntegrationTest`로 동시성 처리 검증
    - `Point` 엔티티에 `version` 필드 추가 및 재구성 로직 수정
    - 관련 서비스, 레포지토리, 엔티티 구현 업데이트

--- apps/commerce-api/src/main/java/com/loopers/domain/point/repository/PointRepository.java
+++ apps/commerce-api/src/main/java/com/loopers/domain/point/repository/PointRepository.java
@@ -2,14 +2,14 @@ package com.loopers.domain.point.repository;
 
 import com.loopers.domain.point.model.Point;
 import java.util.Optional;
-import org.springframework.data.jpa.repository.Lock;
 import org.springframework.stereotype.Component;
 
 @Component
 public interface PointRepository {
 
-    public Optional<Point> findByUserId(String UserId);
+    Optional<Point> findByUserId(String UserId);
 
 
-    public Point save(Point point);
+    Point save(Point point);
+
 }
