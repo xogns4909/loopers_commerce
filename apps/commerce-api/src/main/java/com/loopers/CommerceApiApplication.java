36b84b5a18aaaa402114b2fc1fed313d00e1452e
김태훈 <kth4909@evsis.co.kr>
2025-08-22 08:38:26 +0900

    feat: 결제 상태 스케줄링 처리 추가
    - PENDING 상태 결제 복구를 위한 `PaymentScheduler` 구현
    - 스케줄링 활성화를 위해 `@EnableScheduling` 추가
    - PG 상태 확인 및 이벤트 발행 로직 처리
    - PENDING → SUCCESS/FAILED 전환 및 오류 로깅 개선

--- apps/commerce-api/src/main/java/com/loopers/CommerceApiApplication.java
+++ apps/commerce-api/src/main/java/com/loopers/CommerceApiApplication.java
@@ -6,10 +6,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @ConfigurationPropertiesScan
 @SpringBootApplication
 @EnableFeignClients
+@EnableScheduling
 public class CommerceApiApplication {
 
     @PostConstruct
